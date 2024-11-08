2725270dd4bbb6af5ca22aff06c009c540462dd3
premosipalli <po@veritypay.com>
2024-11-07 18:09:00 -0600

    added preauthorize

--- src/main/java/com/example/authentication/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/authentication/security/ApplicationSecurityConfig.java
@@ -3,7 +3,10 @@ package com.example.authentication.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,9 +21,14 @@ import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static com.example.authentication.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.authentication.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig {
     private final PasswordEncoder passwordEncoder;
 
@@ -32,10 +40,15 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Ensure CSRF is disabled if you don't need it
+//                .csrf(csrf -> csrf.disable()) // Ensure CSRF is disabled if you don't need it
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers("/", "/index.html", "/css/*", "/js/*").permitAll()
                         .requestMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+//                        .requestMatchers("/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
+//                        .requestMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
@@ -60,22 +73,29 @@ public class ApplicationSecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public UserDetailsService userDetailsService(){
         User.UserBuilder user = User.builder()
                 .username("prem")
                 .password(passwordEncoder.encode("prem"))
-                .roles(ApplicationUserRole.STUDENT.name());
+//                .roles(ApplicationUserRole.STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
+                ;
 
         User.UserBuilder user1 = User.builder()
                 .username("kamal")
                 .password(passwordEncoder.encode("kamal"))
-                .roles(ApplicationUserRole.ADMINTRAINEE.name());
+//                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                ;
 
         User.UserBuilder user2 = User.builder()
                 .username("osipalli")
                 .password(passwordEncoder.encode("osipalli"))
-                .roles(ApplicationUserRole.ADMIN.name());
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
+                ;
 
 
         return new InMemoryUserDetailsManager(user.build(),user1.build(), user2.build());
--- src/main/java/com/example/authentication/security/ApplicationUserRole.java
+++ src/main/java/com/example/authentication/security/ApplicationUserRole.java
@@ -1,18 +1,30 @@
 package com.example.authentication.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.authentication.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
     ADMIN(Sets.newHashSet(COURSE_WRITE,COURSE_READ,STUDENT_READ,STUDENT_WRITE)),
-
     ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ));
 
     private final Set<ApplicationUserPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+        return permissions;
+    };
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
